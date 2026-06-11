bc553228ca63c9dd3a75740f113ce3ac39d66c99
papiya dutta <papiyadutta448@gmail.com>
2026-06-11 10:53:22 +0530

    Remove Duplicate From String using Recursuion

---
+++ recursion/removeDuplicate.java
@@ -0,0 +1,26 @@
+package recursion;
+  public class removeDuplicate {
+  public static String deleteDuplicate(String str,int idx,StringBuilder sb,boolean map[]){
+   //base case
+   if(idx==str.length()){
+     return sb.toString();
+
+   }
+   char curr=str.charAt(idx);
+   int mapIndex=curr-'a';
+   if(map[ mapIndex]==true){
+    return deleteDuplicate(str,idx+1,sb,map);
+   }
+   else{
+     map[ mapIndex]=true;
+ return deleteDuplicate(str,idx+1,sb.append(curr),map);
+   }
+   }
+public static void main(String args[]){
+    String str="appnnacollege";
+    boolean map[]=new boolean[26];
+    System.out.println(deleteDuplicate(str,0, new StringBuilder(""),  map));
+}
+  }
+      
+
