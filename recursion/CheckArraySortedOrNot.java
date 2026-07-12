e3d8525054357c331000e9bc0633ae9859593334
papiya dutta <papiyadutta448@gmail.com>
2026-07-12 23:45:32 +0530

    checkarrayissortedornot

---
+++ recursion/CheckArraySortedOrNot.java
@@ -0,0 +1,18 @@
+package recursion;
+public class CheckArraySortedOrNot{
+public static boolean checkSortedornot(int arr[],int element){
+   if(arr.length-1==0||arr.length-1==element){
+  return true;
+   } 
+   if(arr[element]>arr[element+1]){
+    return false;
+   }
+   return  checkSortedornot(arr,element+1);
+   
+}
+public static void main(String args[]){
+    int arr[]={2,4,3,4};
+    boolean result=checkSortedornot(arr,0);
+    System.out.println(result);
+}
+}
