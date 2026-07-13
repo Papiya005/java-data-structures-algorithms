247570e299ad947d59a2e87c548001f131e402ea
papiya dutta <papiyadutta448@gmail.com>
2026-07-13 11:37:23 +0530

    Fix previous mistake

--- recursion/CheckArraySortedOrNot.java
+++ recursion/CheckArraySortedOrNot.java
@@ -4,14 +4,14 @@ public static boolean checkSortedornot(int arr[],int element){
    if(arr.length-1==0||arr.length-1==element){
   return true;
    } 
-   if(arr[element]>arr[element+1]){
+   if(arr[element]>=arr[element+1]){
     return false;
    }
    return  checkSortedornot(arr,element+1);
    
 }
 public static void main(String args[]){
-    int arr[]={2,4,3,4};
+    int arr[]={1,3,3};
     boolean result=checkSortedornot(arr,0);
     System.out.println(result);
 }
