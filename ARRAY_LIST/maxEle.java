a756f16afad170e8928895c80ffb1abb08ee6cd0
papiya dutta <papiyadutta448@gmail.com>
2026-08-05 17:01:12 +0530

    print maxElement using arrayList

---
+++ ARRAY_LIST/maxEle.java
@@ -0,0 +1,23 @@
+package ARRAY_LIST;
+
+import java.util.ArrayList;
+
+public class maxEle {
+    public static void main(String[] args) {
+        ArrayList<Integer>list=new ArrayList<>();
+        int max=Integer.MIN_VALUE;
+        list.add(2);
+         list.add(5);
+          list.add(9);
+           list.add(3);
+            list.add(6);
+            System.err.println(list);
+            for(int i=0;i<list.size();i++){
+                if(max<list.get(i)){
+                    max=list.get(i);
+                }
+            }
+            System.out.print("the max element is "+max);
+    }
+    
+}
