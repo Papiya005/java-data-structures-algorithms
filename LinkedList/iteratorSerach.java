ec830b1ec313ee10b8e1f9fde4e5077ce97d3a30
papiya dutta <papiyadutta448@gmail.com>
2026-08-29 20:17:25 +0530

    linkedList practice

--- LinkedList/iteratorSerach.java
+++ LinkedList/iteratorSerach.java
@@ -72,6 +72,72 @@ head=newnode;
     return -1;
 
   }
+  //recursive search
+  public int helper(node head,int key){
+    if(head==null){
+      return -1;
+
+    }
+    if(head.data==key){
+      return 0;
+
+
+    }
+    int idx=helper(head.next,key);
+    if(idx==-1){
+      return -1;
+    }
+    return idx+1;
+  }
+  public int recSearch(int key){
+    return helper( head, key);
+  }
+  //reverse the likedlist
+  public void reversed(){
+    if(head==null){
+      System.out.println("ll is empty");
+    }
+    
+   node prev=null;
+   node curr=head;
+   node next;
+   while(curr!=null){
+    next=curr.next;
+    curr.next=prev;
+    prev=curr;
+    curr=next;
+    
+
+   }
+   head=prev;
+  
+  }
+
+  //delete from nth node
+  public void deleteNthNode(int n){
+    //calculate the size
+    node temp=head;
+    int sz=0;
+    while(temp!=null){
+      temp=temp.next;
+      sz++;
+    }
+    //  if i have to delete the head node
+    if(n==sz){
+      head=head.next;
+      return;
+    }
+    //sz-n operations
+    int i=1;
+  int idxToFind =sz-n;
+   node prev=head;
+  while(i<idxToFind){
+   prev=prev.next;
+   i++;
+  }
+  prev.next=prev.next.next;
+  return;
+  }
  
  public static void main(String args[]){
 iteratorSerach ll=new iteratorSerach();
@@ -94,6 +160,16 @@ ll.printList();
 System.out.println(size);
 int result=ll.itrSearch(8);
 System.out.println("index at --> "+ result);
+int idx=ll.recSearch(8);
+System.out.println("idx at -->"+idx);
+System.out.println("Before Reversed -->");
+ll.printList();
+ll.reversed();
+System.out.println("After reversed -->");
+ll.printList();
+//delete from the nth node
+ll.deleteNthNode(3);
+ll.printList();
   }
   }
  
